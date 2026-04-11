9d8df361b1712ab792932db3a02110c2dbeb7db0
Muhammad Bayu Ferry Anggoro <62859439+watet18@users.noreply.github.com>
2026-04-11 18:42:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,8 @@ function generateCode() {
     );
   `);
 
+  await pool.query("SELECT setval('videos_id_seq', (SELECT MAX(id) FROM videos))");
+
   await pool.query(`
     CREATE TABLE IF NOT EXISTS admins (
       id BIGINT PRIMARY KEY
